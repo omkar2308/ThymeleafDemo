0659d0cd961fb63c14869d7cc2db6489e7ec5fb8
OMKAR HAJARE <OMKAR HAJARE@LAPTOP-D40P45RS>
2021-05-18 19:25:14 +0530

    updated controller

--- thymeleafDemo/src/main/java/com/example/demo/controller/MyController.java
+++ thymeleafDemo/src/main/java/com/example/demo/controller/MyController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,4 +21,11 @@ public class MyController {
 		return "about";
 	}
 
+	@GetMapping("/loop")
+	public String iteratorHandler(Model m )  {
+		
+		List<String> elements = List.of("akshay","omkar","abhi");
+		m.addAttribute("names", elements);
+		return "iterator";
+	}
 }
